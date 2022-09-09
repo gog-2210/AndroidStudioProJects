a83de0431847cca295f5563ca2304861cefabd86
gog-2210 <guynhuynh123@gmail.com>
2022-09-10 02:18:10 +0700

    finish

--- CuocDuaKyThu/app/src/main/java/com/example/cuocduakythu/MainActivity.java
+++ CuocDuaKyThu/app/src/main/java/com/example/cuocduakythu/MainActivity.java
@@ -6,9 +6,11 @@ import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.ImageButton;
 import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Random;
 
@@ -18,14 +20,16 @@ public class MainActivity extends AppCompatActivity {
     CheckBox cb1,cb2,cb3;
     SeekBar sb1,sb2,sb3;
     ImageButton ibtPlay;
+    int soDiem = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        AnhXa();
-        CountDownTimer countDownTimer = new CountDownTimer(60000,300) {
+        Mapping();
+
+        CountDownTimer countDownTimer = new CountDownTimer(20000,150) {
             @Override
             public void onTick(long l) {
                 int number = 5;
@@ -36,8 +40,47 @@ public class MainActivity extends AppCompatActivity {
                 sb1.setProgress(sb1.getProgress() + n1);
                 sb2.setProgress(sb2.getProgress() + n2);
                 sb3.setProgress(sb3.getProgress() + n3);
-            }
 
+                if(sb1.getProgress() >= sb1.getMax()) {
+                    this.cancel();
+                    ibtPlay.setVisibility(View.VISIBLE);
+                    if(cb1.isChecked()){
+                        soDiem +=10;
+                        Toast.makeText(MainActivity.this, "You WON !!!", Toast.LENGTH_SHORT).show();
+                    }else{
+                        soDiem -=10;
+                        Toast.makeText(MainActivity.this, "You LOST !!!", Toast.LENGTH_SHORT).show();
+                    }
+                    txtDiem.setText(soDiem + "");
+                    EnableTrue();
+                }
+                if(sb2.getProgress() >= sb2.getMax()) {
+                    this.cancel();
+                    ibtPlay.setVisibility(View.VISIBLE);
+                    if(cb2.isChecked()){
+                        soDiem +=10;
+                        Toast.makeText(MainActivity.this, "You WON !!!", Toast.LENGTH_SHORT).show();
+                    }else{
+                        soDiem -=10;
+                        Toast.makeText(MainActivity.this, "You LOST !!!", Toast.LENGTH_SHORT).show();
+                    }
+                    txtDiem.setText(soDiem + "");
+                    EnableTrue();
+                }
+                if(sb3.getProgress() >= sb3.getMax()) {
+                    this.cancel();
+                    ibtPlay.setVisibility(View.VISIBLE);
+                    if(cb3.isChecked()){
+                        soDiem +=10;
+                        Toast.makeText(MainActivity.this, "You WON !!!", Toast.LENGTH_SHORT).show();
+                    }else{
+                        soDiem -=10;
+                        Toast.makeText(MainActivity.this, "You LOST !!!", Toast.LENGTH_SHORT).show();
+                    }
+                    txtDiem.setText(soDiem + "");
+                    EnableTrue();
+                }
+            }
             @Override
             public void onFinish() {
 
@@ -46,13 +89,60 @@ public class MainActivity extends AppCompatActivity {
         ibtPlay.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                ibtPlay.setVisibility(View.INVISIBLE);
-                countDownTimer.start();
+                if(cb1.isChecked() || cb2.isChecked() ||cb3.isChecked()){
+                    ibtPlay.setVisibility(View.INVISIBLE);
+                    sb1.setProgress(0);
+                    sb2.setProgress(0);
+                    sb3.setProgress(0);
+                    cb1.setEnabled(false);
+                    cb2.setEnabled(false);
+                    cb3.setEnabled(false);
+                    sb1.setEnabled(false);
+                    sb2.setEnabled(false);
+                    sb3.setEnabled(false);
+                    countDownTimer.start();
+                }else {
+                    Toast.makeText(MainActivity.this, "Please choose animal do you want to bet before start!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        cb1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(b){
+                    cb2.setChecked(false);
+                    cb3.setChecked(false);
+                }
+            }
+        });
+        cb2.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(b){
+                    cb1.setChecked(false);
+                    cb3.setChecked(false);
+                }
+            }
+        });
+        cb3.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(b){
+                    cb2.setChecked(false);
+                    cb1.setChecked(false);
+                }
             }
         });
     }
-
-    private void AnhXa(){
+    private  void  EnableTrue(){
+        cb1.setEnabled(true);
+        cb2.setEnabled(true);
+        cb3.setEnabled(true);
+        sb1.setEnabled(true);
+        sb2.setEnabled(true);
+        sb3.setEnabled(true);
+    }
+    private void Mapping(){
         txtDiem     = (TextView)    findViewById(R.id.txtDiem);
         ibtPlay     = (ImageButton) findViewById(R.id.ibtPlay);
         cb1         = (CheckBox)    findViewById(R.id.cb1);
